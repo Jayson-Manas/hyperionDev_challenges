63b5a9795352281ab772929fb87cbc86d718e2af
Jayson-Manas <jaysonmanas@gmail.com>
2019-02-21 18:46:17 +0200

    clear screen added

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,10 @@
 var screen = document.getElementById("display");
-
+var calculator = document.getElementById("calculator")
 var values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
+var counter= 1;
 var operators = ["+", "-", "/", "*", "=", "."]
 
+// values
 function number(x){
  
     screen.innerHTML = screen.innerHTML + values[x];
@@ -11,6 +12,7 @@ function number(x){
     
 }
 
+// operations
 function opt(y){
  
     screen.innerHTML = screen.innerHTML + operators[y];
@@ -18,7 +20,29 @@ function opt(y){
     
 }
 
+// evaluating values 
 function operation(){
     screen.innerHTML=eval(screen.innerHTML);
     console.log("pushed")
+}
+
+// hide or show calculator
+function openClose(){
+    if(counter>0){
+        calculator.style.display = "inline";
+    counter--;
+        
+    }else{
+        calculator.style.display ="none";
+        counter++;
+    }
+
+    console.log(counter);
+}
+
+// clear screen
+
+function clearAll(){
+    screen.innerHTML= "";
+console.log("screen cleared")
 }
